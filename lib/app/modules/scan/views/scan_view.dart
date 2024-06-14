19f4608f9ca5f58142b5b04ce8739e62c2e5fbdd
Kingostyl <ariiqmaazin@gmail.com>
2024-06-14 12:57:42 +0700

    update

--- lib/app/modules/scan/views/scan_view.dart
+++ lib/app/modules/scan/views/scan_view.dart
@@ -1,49 +1,37 @@
-// // ignore_for_file: library_private_types_in_public_api, prefer_const_constructors
+// ignore_for_file: library_private_types_in_public_api, prefer_const_constructors, unused_local_variable
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_web_qrcode_scanner/flutter_web_qrcode_scanner.dart';
-// import 'package:get/get.dart';
-// import 'package:uas_flutter/app/routes/app_pages.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:get/get.dart';
+import 'package:uas_flutter/app/routes/app_pages.dart';
 
-// class ScanView extends StatefulWidget {
-//   @override
-//   _ScanViewState createState() => _ScanViewState();
-// }
+class ScanView extends StatefulWidget {
+  @override
+  _ScanViewState createState() => _ScanViewState();
+}
 
-// class _ScanViewState extends State<ScanView> {
-//   CameraController _controller = CameraController(autoPlay: false);
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('QR Code Scanner'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             FlutterWebQrcodeScanner(
-//               cameraDirection: CameraDirection.back,
-//               stopOnFirstResult: false,
-//               controller: _controller,
-//               onGetResult: (result) {
-//                 _controller.stopVideoStream();
-//                      print(result);
-//                      Get.toNamed(Routes.RUANG_LELANG, arguments: {"id_lelang": result});
-//               },
-//               width: 500,
-//               height: 500,
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//                 // Ensure to start or resume the camera stream
-//                 _controller.startVideoStream();
-//               },
-//               child: Text('Start Scanning'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class _ScanViewState extends State<ScanView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('QR Code Scanner'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MobileScanner(
+              onDetect: (capture) {
+                final List<Barcode> barcodes = capture.barcodes;
+                for (final barcode in barcodes) {
+                  print(capture);
+                }
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import google_sign_in_ios
+import mobile_scanner
 import smart_auth
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
 }
